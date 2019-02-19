7adb23e4e037ccb79413e202a7594cc80a42ace2
Praveen Balaji <pvpbalaji@gatech.edu>
2019-02-18 21:53:42 -0500

    Added Character Creation Page buttons, labels and other View entities

---
+++ app/src/main/java/com/example/spacetrader/view/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.spacetrader.view;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.spacetrader.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onStartNewGame(View view){
+        startActivity(new Intent(this,Character_Creation_Activity.class));
+    }
+}
